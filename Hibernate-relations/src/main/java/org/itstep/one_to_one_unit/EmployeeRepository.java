485d941b406e0b9e5f088f20c5cb882c0ac5a522
gk <demar.demidov@yandex.ru>
2024-08-08 20:52:09 +0300

    test

--- Hibernate-relations/src/main/java/org/itstep/one_to_one_unit/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package org.itstep.one_to_one_unit;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-interface EmployeeRepository extends JpaRepository<Employee,Long> {
-} 
